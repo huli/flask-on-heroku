3227b15841070c91497150fec9d67acdbdf47e5e
huli <huli@eitschei.com>
2017-07-16 14:44:24 +0200

    Added route for geolocation.

--- app.py
+++ app.py
@@ -1,13 +1,40 @@
 import os
+import json
+import IP2Location
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import *
+from json import load
+from urllib2 import urlopen
+
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/<nameparameter>')
-def halloWeltAusgeben(nameparameter=None):
+def sayHello(nameparameter=None):
     return render_template('index.html', name=nameparameter)
+
+
+@app.route('/details')
+def details():
+    results = {}
+
+    address = load(urlopen('http://httpbin.org/ip'))['origin']
+    
+    try:
+        address = IPAddress(address)
+    except Exception, e:
+        results['error'] = e.message
+        return Response(json.dumps(results), mimetype='application/json')
+
+    address_details = location.get_all(address)
+    results['ipAddress'] = address
+    results['countryCode'] = address_details.country_short
+    results['countryName'] = address_details.country_long
+    results['cityName'] = address_details.city
+    results['regionName'] = address_details.region
+
+    return Response(json.dumps(results), mimetype='application/json')
 
 
 if __name__ == '__main__':
