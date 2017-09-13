54562e06b55f01ed4bd135bfcb24778632e5dc92
huli <huli@eitschei.com>
2017-09-13 22:18:38 +0200

    Code removed.

--- app.py
+++ app.py
@@ -41,28 +41,6 @@ def details():
 
     return Response(json.dumps(results), mimetype='application/json')
 
-
-@app.route('/footprint')
-def details():
-    results = {}
-
-    address = request.headers.getlist("X-Forwarded-For")[0]
-    
-    try:
-        IP2LocObj = IP2Location.IP2Location()
-        IP2LocObj.open("data/IP2LOCATION-LITE-DB1.BIN")
-        address_details = IP2LocObj.get_all(address)
-    except Exception as e:
-        results['error'] = str(e)
-        return Response(json.dumps(results), mimetype='application/json')
-
-    results['ipAddress'] = address
-    results['countryCode'] = address_details.country_short.decode()
-    results['countryName'] = address_details.country_long.decode()
-
-    return render_template('footprint.html', name=nameparameter)
-
-
 if __name__ == '__main__':
   port = int(os.environ.get('PORT', 5000))
   app.run(host='0.0.0.0', port=port, debug=True)
